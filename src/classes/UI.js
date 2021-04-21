1ed0d04b09e435d54bb34e3c69a8eb8baad980c4
Krostyle <diegoo.albarran@gmail.com>
2021-04-21 02:06:53 -0400

    Se agrega funcionalidad para listar productos agregados a la base de datos sin que se repitan, se soluciona problema en el front

--- src/classes/UI.js
+++ src/classes/UI.js
@@ -3,21 +3,23 @@ export class UI {
         const productList = document.getElementById('product-list')
         const element = document.createElement('div');
         element.innerHTML +=
-            `<div class="card text-center mb-4 mt-4 bg-primary">
-                <div class="card-body">
-                    <strong>Producto</strong>: ${product.name}
-                    <strong>Precio</strong>: ${product.price}
-                    <strong>Cantidad</strong>: ${product.quantity}
-                    <button class="btn btn-danger ml-2 float-right">Eliminar</button>
-                    <button class="btn btn-success float-right">Editar</button>
-                </div>
+            `<div class="card text-center mb-4 bg-primary">                
+                    <div class="card-body">
+                        <strong>Producto</strong>: ${product.name}
+                        <strong>Precio</strong>: ${product.price}
+                        <strong>Cantidad</strong>: ${product.quantity}
+                        <button class="btn btn-danger ml-2 float-right">Eliminar</button>
+                        <button class="btn btn-success float-right">Editar</button>
+                    </div>                
             </div>`;
         productList.appendChild(element);
     }
 
     deleteProductList() {
         const productList = document.getElementById('product-list');
-        productList.replaceChild()
+        while (productList.firstChild) {
+            productList.removeChild(productList.firstChild);
+        }
 
 
 
