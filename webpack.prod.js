2b88a525a60d5de24efce65ff2a42bb194f3c635
Krostyle <diegoo.albarran@gmail.com>
2021-04-26 23:38:08 -0400

    Se elimina Babel, ya que no perimitia el correcto funcionamiento de firebase

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,16 +10,6 @@ module.exports = {
     },
     module: {
         rules: [{
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
                 test: /\.html$/i,
                 loader: 'html-loader',
                 options: {
@@ -39,7 +29,7 @@ module.exports = {
     plugins: [
         new HtmlWPP({
             template: './src/index.html',
-            filename: './index.[contenthash].html'
+            filename: './index.html'
         }),
 
     ]
