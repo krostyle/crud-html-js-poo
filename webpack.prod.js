3de21ec8af09ac83c563cab86981b81bdb8dbcff
Krostyle <diegoo.albarran@gmail.com>
2021-04-26 23:12:33 -0400

    Se añade Babel, se añade hash al index.html, se crear archivo con la configuración de producción de webpack, agrego carpetas y archivos que quitar al .gitignore

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,25 @@
 const HtmlWPP = require('html-webpack-plugin')
 
+
+
 module.exports = {
     mode: 'production',
     output: {
-        filename: 'main.[contenthash].js'
+        filename: 'main.[contenthash].js',
+        clean: true
     },
     module: {
         rules: [{
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
                 test: /\.html$/i,
                 loader: 'html-loader',
                 options: {
@@ -26,8 +39,9 @@ module.exports = {
     plugins: [
         new HtmlWPP({
             template: './src/index.html',
-            filename: './index.html'
-        })
+            filename: './index.[contenthash].html'
+        }),
+
     ]
 
 }
